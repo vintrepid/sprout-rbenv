7436b84ac159c026961c66b7f96387755f554617
Abhi Hiremagalur <abhijit@hiremaga.com>
2014-12-22 08:02:16 -0800

    Only install latest ruby version
    - To speed up provisioning by default
    - People can still opt-in to older rubies by customizing their solistrc

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,8 +2,7 @@ node.default['sprout']['rbenv'] = {
   'home' => "#{node['sprout']['home']}/.rbenv",
   'command' => '/usr/local/bin/rbenv',
   'rubies' => {
-      '1.9.3-p545' => {},
-      '2.0.0-p451' => {},
+      '2.1.5' => {},
     },
-  'default_ruby' => '1.9.3-p545',
+  'default_ruby' => '2.1.5',
 }
